19af1304140ff01644e97dc0aca7301acfd0dd84
sergeychur <churicovser@mail.ru>
2019-03-12 16:26:15 +0300

    port to config

--- server/server.go
+++ server/server.go
@@ -38,23 +38,11 @@ func NewServer(pathToConfig string) (*Server, error) {
 		return nil, err
 	}
 	router := mux.NewRouter()
-	// router.HandleFunc("/users", server.UsersPaginate).Queries("rows", "{rows}", "page", "{page}").Methods(http.MethodGet, http.MethodOptions)
-	// router.HandleFunc("/users/me/", server.GetUser).Methods(http.MethodGet, http.MethodOptions)
-	// router.HandleFunc("/users/me/", server.UpdateUser).Methods(http.MethodPut, http.MethodOptions)
-	// router.HandleFunc("/users/me/", server.DeleteUser).Methods(http.MethodDelete, http.MethodOptions)
-	// router.HandleFunc("/users/", server.LoginAPI).Methods(http.MethodPost, http.MethodOptions)
-	// router.HandleFunc("/session/", server.SignUpAPI).Methods(http.MethodPost, http.MethodOptions)
-	// router.HandleFunc("/session/", server.IsLogin).Methods(http.MethodGet, http.MethodOptions)
-	// router.HandleFunc("/session/", server.Logout).Methods(http.MethodDelete, http.MethodOptions)
-	// router.HandleFunc("/upload/{[0-9]+}", server.UploadAvatar).Methods(http.MethodPost, http.MethodOptions)
-
-	// router.PathPrefix("/files/{.+\\..+$}").Handler(http.StripPrefix("/files/", http.FileServer(http.Dir(server.serverConfig.UploadPath))))
 
 	router.HandleFunc("/users", server.UsersPaginate).Queries("rows", "{rows}", "page", "{page}").Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/users/me/", server.GetUser).Methods(http.MethodGet, http.MethodOptions)       // +
 	router.HandleFunc("/users/me/", server.UpdateUser).Methods(http.MethodPut, http.MethodOptions)    // +
 	router.HandleFunc("/users/me/", server.DeleteUser).Methods(http.MethodDelete, http.MethodOptions) // +
-	// router.HandleFunc("/users/{[0-9]+}", server.GetUser).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/login/", server.LoginAPI).Methods(http.MethodPost, http.MethodOptions)   // +
 	router.HandleFunc("/signup/", server.SignUpAPI).Methods(http.MethodPost, http.MethodOptions) // +
 	router.HandleFunc("/upload/{[0-9]+}", server.UploadAvatar).Methods(http.MethodPost, http.MethodOptions)
@@ -71,7 +59,7 @@ func NewServer(pathToConfig string) (*Server, error) {
 func (server *Server) Run() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8090"
+		port = server.serverConfig.Port
 	} // change for getting from config
 
 	c := cors.New(cors.Options{
