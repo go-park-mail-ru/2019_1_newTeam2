cc446e508a2ebf78112798396511dc98d866ca6b
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-03-13 20:30:24 +0300

    50% test

--- server/session_test.go
+++ server/session_test.go
@@ -14,6 +14,28 @@ type cases struct {
 	Input            string
 }
 
+func TestAlreadySignup(t *testing.T) {
+	server := TestServer()
+	var testCase = cases{
+		ExpectedResponse: 400,
+		Input:            `{"username": "test_user_5", "email": "email@mail.ru", "password": "pass", "langID": 1, "pronounceOn": 1}`,
+	}
+	regRequest, regErr := http.NewRequest("POST", "/users/", strings.NewReader(testCase.Input))
+	if regErr != nil {
+		panic(regErr.Error())
+	}
+	regRequest.Header.Set("Content-Type", "application/json")
+
+	TestResponseRecorder := httptest.NewRecorder()
+	server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/users/", server.SignUpAPI)
+	server.Router.ServeHTTP(TestResponseRecorder, regRequest)
+
+	if TestResponseRecorder.Code != testCase.ExpectedResponse {
+		t.Errorf("Error: expected %v, have %v!\n", testCase.ExpectedResponse, TestResponseRecorder.Code)
+	}
+}
+
 func TestSignup(t *testing.T) {
 	server := TestServer()
 	var testCase = cases{
@@ -41,6 +63,28 @@ func TestLogin(t *testing.T) {
 	var testCase = cases{
 		ExpectedResponse: 200,
 		Input:            `{"username": "test_user_1", "password": "pass"}`,
+	}
+	regRequest, regErr := http.NewRequest("POST", "/session/", strings.NewReader(testCase.Input))
+	if regErr != nil {
+		panic(regErr.Error())
+	}
+	regRequest.Header.Set("Content-Type", "application/json")
+
+	TestResponseRecorder := httptest.NewRecorder()
+	server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/session/", server.LoginAPI)
+	server.Router.ServeHTTP(TestResponseRecorder, regRequest)
+
+	if TestResponseRecorder.Code != testCase.ExpectedResponse {
+		t.Errorf("Error: expected %v, have %v!\n", testCase.ExpectedResponse, TestResponseRecorder.Code)
+	}
+}
+
+func TestBadLogin(t *testing.T) {
+	server := TestServer()
+	var testCase = cases{
+		ExpectedResponse: 401,
+		Input:            `{"username": "test_user_1", "password": "kekpass"}`,
 	}
 	regRequest, regErr := http.NewRequest("POST", "/session/", strings.NewReader(testCase.Input))
 	if regErr != nil {
@@ -102,10 +146,34 @@ func TestLogout(t *testing.T) {
 	}
 }
 
-func TestPositiveCheck(t *testing.T) {
+func TestBadJWTCheck(t *testing.T) {
 	server := TestServer()
 	var testCase = cases{
 		ExpectedResponse: 200,
+		Input:            `{"username": "test_user_1", "password": "pass"}`,
+	}
+	regRequest, regErr := http.NewRequest("GET", "/session/", strings.NewReader(testCase.Input))
+	if regErr != nil {
+		panic(regErr.Error())
+	}
+	regRequest.Header.Set("Content-Type", "application/json")
+
+	regRequest.AddCookie(&http.Cookie{Name: "session_id", Value: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicGFzc3dvcmQiOiJwYXNzIiwidXNlcm5hbWUiOiJ0ZXN0X3VzZXJfMSJ9.Zi01imDsLhyt6NU03BgACX7v2fyiccQsUd1NvdsffgU"})
+
+	TestResponseRecorder := httptest.NewRecorder()
+	server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/session/", server.IsLogin)
+	server.Router.ServeHTTP(TestResponseRecorder, regRequest)
+
+	if TestResponseRecorder.Code != testCase.ExpectedResponse {
+		t.Errorf("Error in \"positive\" IsLogin: expected %v, have %v!\n", testCase.ExpectedResponse, TestResponseRecorder.Code)
+	}
+}
+
+func TestPositiveCheck(t *testing.T) {
+	server := TestServer()
+	var testCase = cases{
+		ExpectedResponse: 204,
 		Input:            `{"username": "test_user_1", "password": "pass"}`,
 	}
 	regRequest, regErr := http.NewRequest("GET", "/session/", strings.NewReader(testCase.Input))
