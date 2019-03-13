a2e68b966051e6bdb2267bd84b3b0b66b783a3fc
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-03-14 00:05:30 +0300

    fix fake db init

--- database/db.go
+++ database/db.go
@@ -1,12 +1,12 @@
 package database
 
 import (
-	"crypto/sha256"
-	"strconv"
+	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/user/2019_1_newTeam2/models"
 	"github.com/user/2019_1_newTeam2/logger"
+	"github.com/user/2019_1_newTeam2/models"
 )
 
 type Database struct {
@@ -14,7 +14,7 @@ type Database struct {
 	// now some map, where we can get users from
 	UserData   map[int]models.User
 	LastUserId int
-	Logger logger.LoggerInterface
+	Logger     logger.LoggerInterface
 }
 
 func NewDataBase() (*Database, error) {
@@ -27,11 +27,15 @@ func NewDataBase() (*Database, error) {
 
 	data := make(map[int]models.User)
 	db.LastUserId = 50
-	h := sha256.New()
-	h.Write([]byte("pass"))
+
+	password := "pass"
+	_, err := HashPassword(password)
+	if err != nil {
+		return nil, fmt.Errorf("hash error")
+	}
 
 	for i := 0; i < db.LastUserId; i++ {
-		data[i] = models.User{i, "test_user_" + strconv.Itoa(i), "kek@lol.kl", string(h.Sum(nil)), 0, 1, 0, "files/avatars/" + strconv.Itoa(i) + ".jpg"}
+		data[i] = models.User{i, "test_user_" + strconv.Itoa(i), "kek@lol.kl", password, 0, 1, 0, "files/avatars/" + strconv.Itoa(i) + ".jpg"}
 	}
 	db.UserData = data
 	return db, nil
