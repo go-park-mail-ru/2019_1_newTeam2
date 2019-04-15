c5594c8019fe440917780abe08bb7a7d7c06f2b7
sergeychur <churicovser@mail.ru>
2019-04-15 09:23:30 +0300

    mutex go away

--- pkg/wshub/clients_map.go
+++ pkg/wshub/clients_map.go
@@ -1,30 +1,19 @@
 package wshub
 
-import (
-	"sync"
-)
-
 type ClientsMap struct {
 	clients map[int]*Client
-	mx sync.RWMutex		// dunno if need
 }
 
 func (m *ClientsMap) Load(key int) (*Client, bool){
-	m.mx.RLock()
-	defer m.mx.RUnlock()
 	val, ok := m.clients[key]
 	return val, ok
 }
 
 func (m * ClientsMap) Store(client *Client) {
-	m.mx.Lock()
-	defer m.mx.Unlock()
 	m.clients[client.ID] = client
 }
 
 func (m *ClientsMap) Delete(key int) {
-	m.mx.Lock()
-	defer m.mx.Unlock()
 	_, ok := m.clients[key]
 	if ok {
 		delete(m.clients, key)
