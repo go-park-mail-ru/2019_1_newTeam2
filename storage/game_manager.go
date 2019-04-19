e87f6e93e7c41aaf91737c9a1e684fb2b25dd846
sergeychur <churicovser@mail.ru>
2019-04-20 01:26:12 +0300

    cookie lives for an hour

--- storage/game_manager.go
+++ storage/game_manager.go
@@ -3,7 +3,7 @@ package storage
 import "github.com/user/2019_1_newTeam2/models"
 
 func (db *Database) UpdateFrequencies (results models.GameResults) (error, bool) {
-	allFound := true
+	/*allFound := true
 	stmt, err := db.Conn.Prepare(UpdateFrequency)
 	if err != nil {
 		return err, false
@@ -11,6 +11,6 @@ func (db *Database) UpdateFrequencies (results models.GameResults) (error, bool)
 	for _,update := range results {
 
 		res, err := stmt.Exec(UpdateFrequency, )
-	}
+	}*/
 	return nil, true
 }
