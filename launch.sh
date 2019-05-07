65bc50e1efd22cd9ad26400ecb1a53577d179820
Tsaanstu <atasunts.vladimir@yandex.ru>
2019-05-08 02:11:38 +0300

    update sh

--- launch.sh
+++ launch.sh
@@ -2,7 +2,9 @@ go build -o api.out ./cmd/api/main.go;
 go build -o auth.out ./cmd/authorization/main.go;
 go build -o chat.out ./cmd/chat/main.go;
 go build -o game.out ./cmd/game/main.go;
+go build -o mgr.out ./cmd/mgr/main.go;
 ./auth.out ./config/config_auth.json &
+./mgr.out ./config/config_score.json &
 ./api.out ./config/config_api.json &
 ./chat.out ./config/config_chat.json &
 ./game.out ./config/config_game.json &
